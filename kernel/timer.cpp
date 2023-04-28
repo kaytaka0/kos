45e9b0169b1d39118a9e8383f0d0cfdab711a2a9
k-takashima <sora3takashima@gmail.com>
2023-04-28 21:16:53 +0900

    day11c

--- kernel/timer.cpp
+++ kernel/timer.cpp
@@ -11,6 +11,8 @@ namespace {
 }
 
 void InitializeLAPICTimer() {
+  timer_manager = new TimerManager;
+  
   divide_config = 0b1011; // divide 1:1
   lvt_timer = (0b010 << 16) | InterruptVector::kLAPICTimer; // not-masked, periodic
   initial_count = kCountMax;
@@ -26,4 +28,14 @@ uint32_t LAPICTimerElapsed() {
 
 void StopLAPICTimer() {
   initial_count = 0;
+}
+
+void TimerManager::Tick() {
+  ++tick_;
+}
+
+TimerManager* timer_manager;
+
+void LAPICTimerOnInterrupt() {
+  timer_manager->Tick();
 }
