ae3654018cc71b838ffc78a5e329fcc3e0664ab5
k-takashima <sora3takashima@gmail.com>
2023-04-28 22:23:20 +0900

    dat11d

--- kernel/timer.cpp
+++ kernel/timer.cpp
@@ -10,12 +10,12 @@ namespace {
   volatile uint32_t& divide_config = *reinterpret_cast<uint32_t*>(0xfee003e0);
 }
 
-void InitializeLAPICTimer() {
-  timer_manager = new TimerManager;
+void InitializeLAPICTimer(std::deque<Message>& msg_queue) {
+  timer_manager = new TimerManager{msg_queue};
   
   divide_config = 0b1011; // divide 1:1
   lvt_timer = (0b010 << 16) | InterruptVector::kLAPICTimer; // not-masked, periodic
-  initial_count = kCountMax;
+  initial_count = 0x1000000u;
 }
 
 void StartLAPICTimer() {
@@ -30,8 +30,31 @@ void StopLAPICTimer() {
   initial_count = 0;
 }
 
+Timer::Timer(unsigned long timeout, int value) : timeout_{timeout}, value_{value} {}
+
 void TimerManager::Tick() {
   ++tick_;
+  while (true) {
+    const auto& t = timers_.top();
+    if (t.Timerout() > tick_) {
+      break;
+    }
+
+    Message m{Message::kTimerTimeout};
+    m.arg.timer.timeout = t.Timerout();
+    m.arg.timer.value = t.Value();
+    msg_queue_.push_back(m);
+
+    timers_.pop();
+  }
+}
+
+TimerManager::TimerManager(std::deque<Message>& msg_queue_) : msg_queue_{msg_queue_} {
+  timers_.push(Timer{std::numeric_limits<unsigned long>::max(), -1});
+}
+
+void TimerManager::AddTimer(const Timer& timer) {
+  timers_.push(timer);
 }
 
 TimerManager* timer_manager;
--- kernel/timer.hpp
+++ kernel/timer.hpp
@@ -1,19 +1,42 @@
 #pragma once
 
 #include <cstdint>
+#include <queue>
+#include <vector>
+#include "message.hpp"
 
-void InitializeLAPICTimer();
+void InitializeLAPICTimer(std::deque<Message>& msg_queue);
 void StartLAPICTimer();
 uint32_t LAPICTimerElapsed();
 void StopLAPICTimer();
 
+class Timer {
+  public:
+  Timer(unsigned long timeout, int value);
+  unsigned long Timerout() const { return timeout_; }
+  int Value() const { return value_; }
+
+  private:
+  unsigned long timeout_;
+  int value_;
+};
+
+// Timerの優先度: タイムアウトが小さいTimerの方が優先度が高い．
+inline bool operator<(const Timer& lhs, const Timer& rhs) {
+  return lhs.Timerout() > rhs.Timerout();
+}
+
 class TimerManager {
   public:
+  TimerManager(std::deque<Message>& msg_queue);
+  void AddTimer(const Timer& timer);
   void Tick();
   unsigned long CurrentTick() const {return tick_;}
 
   private:
-  volatile unsigned int tick_;
+  volatile unsigned long tick_{0};
+  std::priority_queue<Timer> timers_{};
+  std::deque<Message>& msg_queue_;
 };
 
 extern TimerManager* timer_manager;
