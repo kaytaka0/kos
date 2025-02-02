669e5a2b997517ab6d43f78771e4a42a9b7c26a9
takashimakazuki <sora3takashima@gmail.com>
2025-02-02 21:42:05 +0900

    Custom makefile for M1 mac

--- README.md
+++ README.md
@@ -15,3 +15,11 @@
   - X11 Server: opensshの機能を利用
   - X11 Client: XQuartz
   - コード記述: VSCode Remote ssh + ssh
+
+
+
+## Debug
+
+- https://community.platformio.org/t/ld-fails-when-trying-to-create-a-unity-unit-test-on-an-m3-mac-with-platform-native/40072
+
+> Ok, found out the problem - the issue is actually to do with the version of ‘ar’ that is used. The correct version is /usr/bin/ar - normally part of the Mac CommandLineTools. I had installed homebrew binutils which was at the beginning of the path, and the ar included by that causes the problem. The fix (at least to get platformio going properly) was to uninstall the binutils package using ‘brew uninstall binutils’
--- run.sh
+++ run.sh
@@ -1,30 +1,38 @@
 #/bin/bash
 
-set -eo pipefail
+set -e
 
-DIR="$(cd "$( dirname $0 )" && pwd -P)"
+KOS_ROOTDIR="$(cd "$( dirname $0 )" && pwd -P)"
 
 # Build kernel
-echo "====[Build kernel]================================"
-echo "cd $DIR/kernel"
-cd "$DIR/kernel"
+echo "\\n====[Build kernel]====="
+cd "$KOS_ROOTDIR/kernel"
 make
 
 
 # Build the boot loader
-echo "====[Build boot loader]==========================="
-echo "cd $HOME/edk2/"
-cd "$HOME/edk2/"
+echo "\\n====[Setup boot loader]========"
+EDK2_PATH="$HOME/src/github.com/tianocore/edk2"
+LOADER_PATH="$EDK2_PATH/Build/KosLoaderX64/DEBUG_CLANGPDB/X64/KosLoaderPkg/Loader/OUTPUT/Loader.efi"
 
-echo "source edksetup.sh && build"
-source edksetup.sh && build
+if [ -f $LOADER_PATH ]; then
+    LOADER_UPDATED_AT=`date -r $LOADER_PATH`
 
-cp ./Build/KosLoaderX64/DEBUG_CLANG38/X64/Loader.efi $DIR/BOOTX64.EFI
-echo -e "copy BOOTX64.EFI done!!! \n"
+    echo "Boot loader already exists. "
+    echo "$LOADER_PATH: updated at $LOADER_UPDATED_AT"
+    cp $LOADER_PATH $KOS_ROOTDIR/DISK/EFI/BOOT/Loader.efi
+else
+    echo "Building boot loader..."
+    cd $EDK2_PATH
+    pwd
+    build
+    cp $LOADER_PATH $KOS_ROOTDIR/DISK/EFI/BOOT/Loader.efi
+fi
 
 
 # Run QEMU
-echo "====[Run QEMU]===================================="
-echo "cd $DIR"
-cd "$DIR"
-$HOME/osbook/devenv/run_qemu.sh ./BOOTX64.EFI $HOME/kos/kernel/kernel.elf
+echo "\\n====[Run QEMU]======"
+cd "$KOS_ROOTDIR"
+$HOME/src/github.com/uchan-nos/mikanos-build/devenv/run_qemu.sh \
+    $KOS_ROOTDIR/DISK/EFI/BOOT/Loader.efi \
+    $KOS_ROOTDIR/kernel/kernel.elf
