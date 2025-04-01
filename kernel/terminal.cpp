54d6a556ff94ce61b35ced94cadc5ed24e464071
kaytaka0 <sora3takashima@gmail.com>
2025-04-02 02:39:57 +0900

    day16e

--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -20,6 +20,7 @@ Terminal::Terminal() {
         .SetDraggable(true)
         .ID();
     Print(">");
+    cmd_history_.resize(8);
 }
 
 Rectangle<int> Terminal::BlinkCursor() {
@@ -50,7 +51,15 @@ Rectangle<int> Terminal::InputKey(
 
     if (ascii == '\n') {
         linebuf_[linebuf_index_] = 0;
+        
+        // コマンド履歴保存
+        if (linebuf_index_ > 0) {
+            cmd_history_.pop_back();
+            cmd_history_.push_front(linebuf_);
+        }
+        
         linebuf_index_ = 0;
+        cmd_history_index_ = -1; // Enter 入力直後は履歴の index をリセット
         cursor_.x = 0;
         if (cursor_.y < kRow - 1) {
             ++cursor_.y;
@@ -78,6 +87,10 @@ Rectangle<int> Terminal::InputKey(
             WriteAscii(*window_->Writer(), CalcCursorPos(), ascii, {255, 255, 255} );
             ++cursor_.x;
         }
+    } else if (keycode == 0x51) { // down arrow
+        draw_area = HistoryUpDown(-1);
+    } else if (keycode == 0x52) { // up arrow
+        draw_area = HistoryUpDown(1);
     }
     
     DrawCursor(true);
@@ -158,6 +171,32 @@ void Terminal::Print(const char* s) {
     }
     
     DrawCursor(true);
+}
+
+Rectangle<int> Terminal::HistoryUpDown(int direction) {
+    if (direction == -1 && cmd_history_index_ >= 0) {
+        --cmd_history_index_;
+    } else if (direction == 1 && cmd_history_index_ +1 < cmd_history_.size()) {
+        ++cmd_history_index_;
+    }
+    
+    cursor_.x = 1;
+    const auto first_pos = CalcCursorPos();
+
+    Rectangle<int> draw_area{first_pos, {8*(kColumns - 1), 16}};
+    FillRectangle(*window_->Writer(), draw_area.pos, draw_area.size, {0, 0, 0});
+
+    const char* history = "";
+    if (cmd_history_index_ >= 0) {
+        history = &cmd_history_[cmd_history_index_][0];
+    }
+    
+    strcpy(&linebuf_[0], history);
+    linebuf_index_ = strlen(history);
+
+    WriteString(*window_->Writer(), first_pos, history, {255, 255, 255});
+    cursor_.x = linebuf_index_ + 1;
+    return draw_area;
 }
 
 
